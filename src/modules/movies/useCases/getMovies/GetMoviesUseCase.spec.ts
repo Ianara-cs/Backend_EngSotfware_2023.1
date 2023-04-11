4f1230035df76b4dec11b1132fb19c5265a36b37
Ianara-cs <ianarasilva333@gmail.com>
2023-04-10 23:48:38 -0300

    testes unitários

---
+++ src/modules/movies/useCases/getMovies/GetMoviesUseCase.spec.ts
@@ -0,0 +1,34 @@
+import { MoviesRepositoryInMemory } from "../../repositories/in-memory/MoviesRepositoryInMemory"
+import { GetMoviesUseCase } from "./GetMoviesUseCase"
+
+let moviesRepositoryInMemory: MoviesRepositoryInMemory
+let getMoviesUseCase: GetMoviesUseCase
+
+describe("Get Movies", () => {
+    beforeEach(() => {
+        moviesRepositoryInMemory = new MoviesRepositoryInMemory()
+        getMoviesUseCase = new GetMoviesUseCase(moviesRepositoryInMemory)
+    })
+
+    it("should be able to get a list of movies", async() => {
+        await moviesRepositoryInMemory.create({
+            name: "Sorria",
+	        description: "Após um paciente cometer um suicídio brutal em sua frente, a psiquiatra Rose é perseguida por uma entidade maligna que muda de forma.",
+	        url_image: "",
+	        gere: "Terror",
+	        year: 2022  
+        })
+
+        await moviesRepositoryInMemory.create({
+            name: "Eternos",
+	        description: "descrição.",
+	        url_image: "",
+	        gere: "Ação",
+	        year: 2021 
+        }) 
+
+        const movies = await getMoviesUseCase.execute()
+        expect(movies.length).toEqual(2)
+        expect(movies[0]).toHaveProperty("id")
+    })
+})
